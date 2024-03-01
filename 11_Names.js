49282170a36b21037382473b0b5e09a8a59c1a83
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 11:59:44 +0500

    initial commit

---
+++ 11_Names.js
@@ -0,0 +1,9 @@
+"use strict";
+let names = ['Uzair', 'Umair', 'Faisal', 'Bilal', 'Saad'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+// let dishes: string[] = ['Chicken', 'Burger', 'Zinger', 'Broast', 'Fishes', 'Roll', 'Samosa'];
+// for(let i=0; i<dishes.length; i++){
+//     console.log(dishes[i]);
+// }
